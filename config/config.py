4b55594a2810b52293cb3f1cbdb716dcbb9fbcce
LBM2020 <52284157+LBM2020@users.noreply.github.com>
2021-03-23 20:19:15 +0800

    Create config.py

---
+++ config/config.py
@@ -0,0 +1,18 @@
+opt = {
+
+    #路径相关
+    'model_output_path':'xxx',
+    'train_filepath':'xxx',
+    'valid_filepath':'xxx',
+    'word2vec_filepath':'xxx',
+
+    #模型参数相关
+    'epoches':'x',
+    'input_dim':'x',
+    'hidden_dim':'x',
+    'num_class':'x',
+    'num_layer':'x',
+    'batch_size':'x',
+    'target_names':['xxx', 'xxx'],
+    'seed':'x'
+}
